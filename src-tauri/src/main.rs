2934ef1cdbf7818129dfe9c2a8c5837f79e87cdf
Yu Chen <yu.chen@live.ie>
2024-06-25 23:39:54 +0800

    macos tested

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::menu::{MenuItemBuilder, MenuItemKind, PredefinedMenuItem};
+use base64::{engine::general_purpose::STANDARD, Engine as _};
+use tauri::{
+    menu::{CheckMenuItemBuilder, MenuId, MenuItemKind, PredefinedMenuItem},
+    AppHandle, Manager,
+};
+use tauri_plugin_deep_link::DeepLinkExt;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -24,29 +29,204 @@ fn md_to_html(md: &str) -> String {
     comrak::markdown_to_html(md, &options)
 }
 
-fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![md_to_html])
-        .setup(|app| {
-            #[cfg(target_os = "macos")]
-            if let Some(menu) = app.menu() {
-                if let Ok(items) = menu.items() {
-                    for item in &items {
-                        println!("{:#?}", item.id());
-                    }
-                    let separator = PredefinedMenuItem::separator(app)?;
-                    let open = MenuItemBuilder::with_id("open", "Open")
-                        .accelerator("CmdOrCtrl+O")
-                        .build(app)?;
-                    if let Some(MenuItemKind::Submenu(file_menu)) = items.get(1) {
-                        file_menu.insert(&separator, 0)?;
-                        file_menu.insert(&open, 0)?;
+#[cfg(any(target_os = "windows", target_os = "linux"))]
+fn args() -> Vec<String> {
+    let args = std::env::args();
+    args.into_iter().collect()
+}
+
+#[tauri::command]
+fn url_to_path(url: &str) -> String {
+    if let Ok(url) = url::Url::parse(url) {
+        if let Ok(url) = url.to_file_path() {
+            if let Some(url) = url.to_str() {
+                return url.to_string();
+            }
+        }
+    }
+    "".to_string()
+}
+
+#[tauri::command]
+fn join(url: &str, path: &str) -> String {
+    if let Ok(url) = url::Url::parse(url) {
+        if let Ok(url) = url.join(path) {
+            return url.to_string();
+        }
+    }
+    "".to_string()
+}
+
+#[tauri::command]
+fn decode_base64(base64: &str) -> String {
+    if let Ok(v) = STANDARD.decode(base64) {
+        if let Ok(s) = String::from_utf8(v) {
+            return s;
+        }
+    }
+    "".to_string()
+}
+
+#[cfg(target_os = "macos")]
+fn check_win_menu(app: &AppHandle, id: &MenuId) {
+    if let Some(menu) = app.menu() {
+        if let Some(MenuItemKind::Submenu(ref win_menu)) = menu.get(tauri::menu::WINDOW_SUBMENU_ID)
+        {
+            if let Ok(items) = win_menu.items() {
+                for item in items {
+                    if let MenuItemKind::Check(item) = item {
+                        item.set_checked(item.id() == id).unwrap();
                     }
                 }
             }
+        }
+    }
+}
+
+#[cfg(target_os = "macos")]
+fn add_win_menu(app: &AppHandle, label: &str) {
+    let title = if label == "" {
+        "MD Viewer".to_string()
+    } else {
+        url_to_path(label)
+    };
+    let label = STANDARD.encode(label);
+    if let Some(win) = app.get_webview_window(&label) {
+        let _ = win.set_focus();
+        let id = format!("window-{}", &label);
+        let id = MenuId::new(id);
+        check_win_menu(app, &id);
+        return;
+    }
+    let win =
+        tauri::WebviewWindowBuilder::new(app, &label, tauri::WebviewUrl::App("index.html".into()))
+            .inner_size(800.0, 600.0)
+            .title(&title)
+            .build()
+            .unwrap();
+    if let Some(menu) = app.menu() {
+        if let Some(MenuItemKind::Submenu(ref win_menu)) = menu.get(tauri::menu::WINDOW_SUBMENU_ID)
+        {
+            let id = format!("window-{}", &label);
+            let menu = CheckMenuItemBuilder::with_id(&id, &title)
+                .build(app)
+                .unwrap();
+            win_menu.append(&menu).unwrap();
+            check_win_menu(app, menu.id());
+
+            win.on_window_event(move |e| match e {
+                tauri::WindowEvent::Destroyed => {
+                    remove_win_menu(menu.app_handle(), menu.id());
+                }
+                tauri::WindowEvent::Focused(focused) => {
+                    if *focused {
+                        check_win_menu(menu.app_handle(), menu.id());
+                    }
+                }
+                _ => {}
+            });
+        }
+    }
+}
+
+#[cfg(any(target_os = "windows", target_os = "linux"))]
+fn add_win(app: &AppHandle, label: &str) {
+    let title = if label == "" {
+        "MD Viewer".to_string()
+    } else {
+        url_to_path(label)
+    };
+    let label = STANDARD.encode(label);
+    tauri::WebviewWindowBuilder::new(app, &label, tauri::WebviewUrl::App("index.html".into()))
+        .inner_size(800.0, 600.0)
+        .title(&title)
+        .build()
+        .unwrap();
+}
+
+#[cfg(target_os = "macos")]
+fn remove_win_menu(app: &AppHandle, id: &MenuId) {
+    if let Some(menu) = app.menu() {
+        if let Some(MenuItemKind::Submenu(ref win_menu)) = menu.get(tauri::menu::WINDOW_SUBMENU_ID)
+        {
+            if let Ok(items) = win_menu.items() {
+                for item in items {
+                    if let MenuItemKind::Check(item) = item {
+                        if item.id() == id {
+                            win_menu.remove(&item).unwrap();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_deep_link::init())
+        .plugin(tauri_plugin_fs::init())
+        .invoke_handler(tauri::generate_handler![
+            md_to_html,
+            url_to_path,
+            join,
+            decode_base64
+        ])
+        .setup(|app| {
+            #[cfg(any(target_os = "windows", target_os = "linux"))]
+            {
+                let args = args();
+                if args.len() > 1 {
+                    if let Ok(url) = url::Url::from_file_path(&args[1]) {
+                        let url = url.to_string();
+                        add_win(app.handle(), &url);
+                    }
+                }
+            }
+
+            #[cfg(target_os = "macos")]
+            {
+                if let Some(menu) = app.menu() {
+                    if let Some(MenuItemKind::Submenu(win_menu)) =
+                        menu.get(tauri::menu::WINDOW_SUBMENU_ID)
+                    {
+                        let separator = PredefinedMenuItem::separator(app)?;
+                        win_menu.insert(&separator, 4)?;
+                    }
+                }
+
+                let deep_link = app.deep_link();
+                if let Ok(Some(urls)) = deep_link.get_current() {
+                    for url in urls {
+                        let url = url.to_string();
+                        add_win_menu(app.handle(), &url);
+                    }
+                } else {
+                    add_win_menu(app.handle(), "");
+                }
+
+                app.on_menu_event(move |app, event| {
+                    let id = event.id();
+                    if id.0.starts_with("window-") {
+                        check_win_menu(app, id);
+                        let label = &id.0[7..];
+                        if let Some(win) = app.get_webview_window(label) {
+                            let _ = win.set_focus();
+                        }
+                    }
+                });
+            }
             Ok(())
         })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        .run(|app, event| {
+            #[cfg(target_os = "macos")]
+            if let tauri::RunEvent::Opened { urls } = event {
+                for url in urls {
+                    let url = url.to_string();
+                    add_win_menu(app, &url);
+                }
+            }
+        });
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,54 @@
 <script lang="ts">
   import { onMount, tick } from "svelte";
   import "./index.scss";
-  import { invoke } from "@tauri-apps/api/core";
+  import { convertFileSrc, invoke } from "@tauri-apps/api/core";
   import katex from "katex";
   import hljs from "highlight.js";
-  import md from "./example.md?raw";
-  import { app } from "@tauri-apps/api";
+  import defaultMd from "./example.md?raw";
   import { getCurrent, type Theme } from "@tauri-apps/api/window";
+  import { readTextFile } from "@tauri-apps/plugin-fs";
 
   let html = "";
   let nav: HTMLElement;
   let navWidth = 200;
   let theme: Theme = "light";
+  let args = "";
 
-  async function md_to_html() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  async function md_to_html(md: string) {
     html = await invoke("md_to_html", {
       md,
     });
   }
 
+  function url_to_path(url: string): Promise<string> {
+    return invoke("url_to_path", {
+      url,
+    });
+  }
+
+  function join(url: string, path: string): Promise<string> {
+    return invoke("join", {
+      url,
+      path,
+    });
+  }
+
+  function decode_base64(base64: string): Promise<string> {
+    return invoke("decode_base64", {
+      base64,
+    });
+  }
+
   onMount(async () => {
     const w = getCurrent();
+    console.log(w.label);
+    let md = defaultMd;
+    let url = await decode_base64(w.label);
+    if (url) {
+      const path = await url_to_path(url);
+      md = await readTextFile(path);
+    }
+
     theme = (await w.theme()) || "light";
     document.body.className = theme;
 
@@ -29,7 +56,8 @@
       theme = e.payload;
       document.body.className = theme;
     });
-    await md_to_html();
+
+    await md_to_html(md);
     await tick();
     let anchors = document.querySelectorAll(
       '[id^="nav-item-"]',
@@ -52,6 +80,19 @@
         a.target = "_blank";
       }
     }
+
+    if (url) {
+      const imgs = document.querySelectorAll("img");
+      for (const img of imgs.values()) {
+        const src = img.getAttribute("src");
+        if (src && !src.startsWith("http")) {
+          const furl = await join(url, src);
+          const path = await url_to_path(furl);
+          img.src = convertFileSrc(path);
+        }
+      }
+    }
+
     const maths = document.querySelectorAll("[data-math-style]");
     maths.forEach((e) => {
       try {
@@ -67,6 +108,7 @@
 <nav style="width: {navWidth}px;" bind:this={nav}></nav>
 
 <aside style="margin-left: {navWidth + 36}px;">
+  <div>{args}</div>
   <main>
     {@html html}
   </main>
